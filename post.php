4c2e480469fbf15416d17b98152e8f5d8d0a8ccb
ajswebdesigns <andrew@ajswebdesigns.com>
2022-07-17 16:32:38 -0400

    Lesson 312 complete and operational

--- post.php
+++ post.php
@@ -1,8 +1,14 @@
     <!-- Header -->
-    <?php include './includes/db.php' ?>
-    <?php include './includes/header.php' ?>
+    <?php include './includes/db.php'?>
+    <?php include './includes/header.php'?>
     <!-- Navigation -->
-    <?php include './includes/navigation.php' ?>
+    <?php include './includes/navigation.php'?>
+<?php
+if (isset($_POST['liked'])) {
+    echo 'it works';
+}
+
+?>
 
     <!-- Page Content -->
     <div class="container">
@@ -14,34 +20,31 @@
 
                 <?php
 
-                if (isset($_GET['p_id'])) {
-                    $the_post_id = $_GET['p_id'];
+if (isset($_GET['p_id'])) {
+    $the_post_id = $_GET['p_id'];
 
-               $view_query = "UPDATE posts SET post_views_count = post_views_count +1 WHERE post_id =$the_post_id ";
-               $send_query = mysqli_query($connection,$view_query);
+    $view_query = "UPDATE posts SET post_views_count = post_views_count +1 WHERE post_id =$the_post_id ";
+    $send_query = mysqli_query($connection, $view_query);
 
-if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
-  $query = "SELECT * FROM posts WHERE post_id = {$the_post_id}";
-} else {
-      $query = "SELECT * FROM posts WHERE post_id = {$the_post_id} AND post_status = 'published'";
-}
+    if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin') {
+        $query = "SELECT * FROM posts WHERE post_id = {$the_post_id}";
+    } else {
+        $query = "SELECT * FROM posts WHERE post_id = {$the_post_id} AND post_status = 'published'";
+    }
 
-                $select_all_posts_query = mysqli_query($connection, $query);
-                if(mysqli_num_rows($select_all_posts_query)<1 ) {
-                    echo 'No Posts Available';
-                } else {
+    $select_all_posts_query = mysqli_query($connection, $query);
+    if (mysqli_num_rows($select_all_posts_query) < 1) {
+        echo 'No Posts Available';
+    } else {
 
-
-
-                
-                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                    // var_dump($row);
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
-                ?>
+        while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+            // var_dump($row);
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            ?>
 
                     <h1 class="page-header">
                         Page Heading
@@ -50,55 +53,62 @@ if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
 
                     <!-- First Blog Post -->
                     <h2>
-                        <a href="#"><?= $post_title ?></a>
+                        <a href="#"><?=$post_title?></a>
                     </h2>
                     <p class="lead">
-                        by <a href="index.php"><?= $post_author ?></a>
+                        by <a href="index.php"><?=$post_author?></a>
                     </p>
-                    <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
+                    <p><span class="glyphicon glyphicon-time"></span><?=$post_date?></p>
                     <hr>
                     <img class="img-responsive" src="/images/<?php echo $post_image ?>" alt="">
                     <hr>
-                    <p><?= $post_content ?></p>
+                    <p><?=$post_content?></p>
                     <!-- <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a> -->
 
                     <hr>
-                <?php }} } else {
-                    header('Location: index.php');
-                }
 
-                ?>
+                        <div class="row">
+                            <p class="pull-right"><a class="like-btn" href="#"><span class="glyphicon glyphicon-thumbs-up"></span> Like</a></p>
+                        </div>
+                          <div class="row">
+                            <p class="pull-right">Like: 10</p>
+                        </div>
+                        <div class="clearfix">
+
+                        </div>
+                <?php }}} else {
+    header('Location: index.php');
+}
+
+?>
 
                 <!-- Blog Comments -->
 
                 <?php
-                if (isset($_POST['create_comment'])) {
-                    $the_post_id = $_GET['p_id'];
-                    $comment_author = $_POST['comment_author'];
-                    $comment_email = $_POST['comment_email'];
-                    $comment_content = $_POST['comment_content'];
-                    if (!empty($comment_author && $comment_email &&$comment_content)) {
-                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-                        $query .= "VALUES($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
-                        $create_comment_query = mysqli_query($connection, $query);
-                        if (!$create_comment_query) {
-                            die('Query Failed' . mysqli_error($connection));
-                        }
+if (isset($_POST['create_comment'])) {
+    $the_post_id = $_GET['p_id'];
+    $comment_author = $_POST['comment_author'];
+    $comment_email = $_POST['comment_email'];
+    $comment_content = $_POST['comment_content'];
+    if (!empty($comment_author && $comment_email && $comment_content)) {
+        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+        $query .= "VALUES($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
+        $create_comment_query = mysqli_query($connection, $query);
+        if (!$create_comment_query) {
+            die('Query Failed' . mysqli_error($connection));
+        }
 
+        // $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+        // $query .= "WHERE post_id = $the_post_id ";
+        // $update_comment_count = mysqli_query($connection, $query);
 
-                        // $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                        // $query .= "WHERE post_id = $the_post_id ";
-                        // $update_comment_count = mysqli_query($connection, $query);
+    } else {
+        echo "<script>alert('Fields Cannot Be Empty')</script>";
+    }
+    header("Location: post.php?p_id=$the_post_id");
+}
 
-
-                        
-                    } else {
-                        echo "<script>alert('Fields Cannot Be Empty')</script>";
-                    }
-                    header("Location: post.php?p_id=$the_post_id"); 
-                }
-
-                ?>
+?>
 
                 <!-- Comments Form -->
                 <div class="well">
@@ -127,17 +137,17 @@ if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
                 <!-- Posted Comments -->
                 <?php
 
-                $query = "SELECT * FROM comments WHERE comment_post_id = $the_post_id ";
-                $query .= "AND comment_status = 'approved' ";
-                $query .= "ORDER BY comment_id DESC ";
-                $select_comment_query = mysqli_query($connection, $query);
-                if (!$select_comment_query) {
-                    die('Query has failed' . mysqli_error($connection));
-                }
-                while ($row = mysqli_fetch_array($select_comment_query)) {
-                    $comment_date = $row['comment_date'];
-                    $comment_content = $row['comment_content'];
-                    $comment_author = $row['comment_author']; ?>
+$query = "SELECT * FROM comments WHERE comment_post_id = $the_post_id ";
+$query .= "AND comment_status = 'approved' ";
+$query .= "ORDER BY comment_id DESC ";
+$select_comment_query = mysqli_query($connection, $query);
+if (!$select_comment_query) {
+    die('Query has failed' . mysqli_error($connection));
+}
+while ($row = mysqli_fetch_array($select_comment_query)) {
+    $comment_date = $row['comment_date'];
+    $comment_content = $row['comment_content'];
+    $comment_author = $row['comment_author'];?>
 
                     <!-- Comment -->
                     <div class="media">
@@ -145,18 +155,52 @@ if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
                             <img class="media-object" src="https://via.placeholder.com/64" alt="">
                         </a>
                         <div class="media-body">
-                            <h4 class="media-heading"><?= $comment_author; ?>
-                                <small><?= $comment_date; ?></small>
+                            <h4 class="media-heading"><?=$comment_author;?>
+                                <small><?=$comment_date;?></small>
                             </h4>
-                            <p><?= $comment_content; ?></p>
+                            <p><?=$comment_content;?></p>
                         </div>
                     </div>
 
-                <?php } ?>
+                <?php }?>
             </div>
             <!-- Blog Sidebar Widgets Column -->
-            <?php include './includes/sidebar.php'  ?>
+            <?php include './includes/sidebar.php'?>
         </div>
         <!-- /.row -->
         <hr>
-        <?php include './includes/footer.php'  ?>
+        <?php include './includes/footer.php'?>
+<script>
+
+  $(document).ready(function() {
+      //for use later . . .
+      var post_id = <?php echo $the_post_id; ?>;
+      //hardcoded #
+      var user_id = 94;
+
+    $('.like-btn').click(function() {
+
+        //create AJAX function
+        $.ajax({
+          url: "post.php?p_id=<?php echo $the_post_id; ?>",
+          type: 'post',
+          data: {
+            liked: 1,
+            /* '' simply differentiate key from value;
+              'post_id' value for 'type:post' */
+            post_id: post_id,
+            // hardcoded user_id from var above
+            user_id: user_id
+          },
+          //see if successful . . .
+          success: function(data) {
+           alert(data);
+          }
+
+        });
+
+    });
+
+  });
+</script>
+
